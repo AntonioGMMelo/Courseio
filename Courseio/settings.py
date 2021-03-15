e765fad69ebd5cfe334ef3e9d8fdd9aed9f0b652
Antônio Gabriel Malé de Melo <73307544+AntonioGMMelo@users.noreply.github.com>
2021-03-15 15:47:04 +0000

    Update settings.py

--- Courseio/settings.py
+++ Courseio/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i5*l9p_ds(auw3g7)sl_3h_ex#kfw^nxb8fms%sv^14^^j0ilu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
